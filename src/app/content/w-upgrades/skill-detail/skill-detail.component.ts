b150ce54237f1dfc9b54b0eda3604b8f91d621b6
sprtokiller <sprtokiller.6c@gmail.com>
2019-05-10 22:33:47 +0200

    skillTile

---
+++ src/app/content/w-upgrades/skill-detail/skill-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { SkillTile } from '../srcSkills/skillTile';
+import { CestyService } from '../../../cesty.service';
+@Component({
+  selector: 'app-skill-detail',
+  templateUrl: './skill-detail.component.html',
+  styleUrls: ['./skill-detail.component.css']
+})
+export class SkillDetailComponent implements OnInit {
+  @Input() skillTile: SkillTile;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/content/w-upgrades/w-upgrades.component.ts
+++ src/app/content/w-upgrades/w-upgrades.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SKILLTILES } from './srcPlayers/MOCK_SKILL_TILES';
+import { PlayerTile } from './srcPlayers/playerTile';
 @Component({
   selector: 'app-w-upgrades',
   templateUrl: './w-upgrades.component.html',
   styleUrls: ['./w-upgrades.component.css']
 })
 export class WUpgradesComponent implements OnInit {
-
+  sTiles = PLAYERTILES;
   constructor() { }
 
   ngOnInit() {
