ae3a52ab1593310e3bcfbc149142cace958e81ef
Feoras1 <Tomas.svondr@student.sps-cl.cz>
2019-05-15 14:29:05 +0200

    Svondi

--- src/app/content/w-stats/w-stats.component.ts
+++ src/app/content/w-stats/w-stats.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Zprava } from './zprava';
 @Component({
   selector: 'app-w-stats',
   templateUrl: './w-stats.component.html',
   styleUrls: ['./w-stats.component.css']
 })
 export class WStatsComponent implements OnInit {
+UName:string="DickObraz";
+UBalance:number=912093210941094;
+z1:Zprava = {id: 1, text: "oof", color: "red" };
+z2:Zprava = {id: 1, text: "oof", color: "white" };
+z3:Zprava = {id: 1, text: "oof", color: "white" };
+Zpravas: Zprava[] = [this.z1, this.z2, this.z3];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+ 
 }
---
+++ src/app/content/w-stats/zprava.ts
@@ -0,0 +1,5 @@
+export class Zprava {
+    id: number;
+    text: string;
+    color: string;
+  }
