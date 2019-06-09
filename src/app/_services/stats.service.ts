3976ab4b493797c4360ca7d6bab00ef4a32bc28b
sprtokiller <sprtokiller.6c@gmail.com>
2019-06-09 17:30:12 +0200

    login from tutorial, icon & login graphics

---
+++ src/app/_services/stats.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Zprava } from '../game/w-stats/zprava'
+@Injectable({
+  providedIn: 'root'
+})
+export class StatsService {
+  private messagesSource = new Subject<void>();
+  public messages$ = this.messagesSource.asObservable();
+  public msg1: Zprava = {id: 0, text: "", color: "black"}
+  public msg2: Zprava = {id: 1, text: "Your offensive branch is: FIRE", color: "Red"}
+  public msg3: Zprava = {id: 2, text: "Your defensive branch is: Armor", color: "RoyalBlue"}
+  public nextID: number = 3;
+  constructor() {
+    
+  }
+  pushMsg(msgIn : Zprava){
+    this.nextID++;
+    this.msg1 = this.msg2;
+    this.msg2 = this.msg3;
+    this.msg3 = msgIn;
+    console.log(msgIn.text);
+    this.messagesSource.next();
+  }
+  msgObservable() {
+  //  return this.msgs.asObservable();
+  //  return this.test.
+
+  }
+}
