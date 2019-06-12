a84318e53438f630b8ccc892a8af37fadfabd0f1
sprtokiller <sprtokiller.6c@gmail.com>
2019-06-12 18:24:01 +0200

    click handled clicker

--- src/app/game/w-clicker/w-clicker.component.ts
+++ src/app/game/w-clicker/w-clicker.component.ts
@@ -10,21 +10,49 @@ export class WClickerComponent implements OnInit {
   constructor() { }
 
   event: MouseEvent;
-  posX = 0;
-  posY = 0;
+  clicks = 0;
+  posX = null;
+  posY = null;
+  posXP = null;
+  posYP = null;
+  posXPs = "NaN";
+  posYPs = "Nan";
+  divX = 0;
+  divY = 0;
   divNextX = 0;
   divNextY = 0;
+  divNextXs = "NaN";
+  divNextYs = "NaN";
   onClick(event: MouseEvent): void {
   //  this.event = event;
-  //  this.posX = event.clientX;
-  //  this.posY = event.clientY;
+  this.posX = event.clientX;
+  this.posY = event.clientY;
+  this.divX = document.getElementById("clickerZone").offsetWidth;
+  this.divY = document.getElementById("clickerZone").offsetHeight;
+  
+  this.posXP = (this.posX / this.divX * 100);
+  this.posYP = (this.posY / this.divY * 100);
+
+  this.posXPs = this.posXP.toString().substr(0, 5);
+  this.posYPs = this.posYP.toString().substr(0, 5);
+  this.clicks++;
   //  console.log("Click:" + this.posX.toString() + "x " + this.posY.toString() + "y")
   this.divNextX = Math.random() * 100; 
-  this.divNextY = Math.random() * 100; 
+  this.divNextY = Math.random() * 100;
 
+  this.divNextXs = this.divNextX.toString().substr(0, 5);
+  this.divNextYs = this.divNextY.toString().substr(0, 5);
+  console.log(event.clientX + " " + event.clientY);
 }
   ngOnInit() {
-    document.getElementById("canvas");
+    this.divX = document.getElementById("clickerZone").offsetWidth;
+    this.divY = document.getElementById("clickerZone").offsetHeight;
+
+    this.divNextX = Math.random() * 100; 
+    this.divNextY = Math.random() * 100;
+
+    this.divNextXs = this.divNextX.toString().substr(0, 5);
+    this.divNextYs = this.divNextY.toString().substr(0, 5);
   }
   
 }
