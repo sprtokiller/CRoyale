9dd37554bf88e6742ee39791a0d27d4404f6dc3d
sprtokiller <sprtokiller.6c@gmail.com>
2019-06-20 15:53:57 +0200

    titlePicker 1

---
+++ src/app/lobby/login/localResx/GBTNS.ts
@@ -0,0 +1,19 @@
+import { GBtn } from './gBtn';
+
+export const GBTNS: GBtn[] = [
+  {
+    "id": 0,
+    "caption": "SOLO",
+    "selected": true
+  },
+  {
+    "id": 1,
+    "caption": "DUO",
+    "selected": false
+  },
+  {
+    "id": 2,
+    "caption": "SQUAD",
+    "selected": false
+  },
+];
